4b23257cc9dda80d002797fa9e72cf6031868308
Tarun Yadvendu <tarun.yadvendu@gmail.com>
2023-09-22 23:22:08 +0530

    understanding mutablestateflow

--- app/src/main/java/com/whis/repository/ExerciseListRepository.kt
+++ app/src/main/java/com/whis/repository/ExerciseListRepository.kt
@@ -3,11 +3,7 @@ package com.whis.repository
 import com.whis.BuildConfig
 import com.whis.Network.ApiServices
 import com.whis.model.ExerciseListBean
-import com.whis.model.WorkoutListBean
-import com.whis.utils.API_KEY
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.withContext
 import java.util.HashMap
 import javax.inject.Inject
--- app/src/main/java/com/whis/repository/WorkoutRemoveRepository.kt
+++ app/src/main/java/com/whis/repository/WorkoutRemoveRepository.kt
@@ -2,13 +2,8 @@ package com.whis.repository
 
 import com.whis.BuildConfig
 import com.whis.Network.ApiServices
-import com.whis.model.WorkoutAddBean
-import com.whis.model.WorkoutListBean
 import com.whis.model.WorkoutRemoveBean
-import com.whis.utils.API_KEY
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.withContext
 import java.util.HashMap
 import javax.inject.Inject
